913adbacf023677cacdf30f46ac80c1f4ed1fecd
Protom512 <ghouno05@gmail.com>
2013-11-16 20:07:23 +0900

    section
    setting part still not working.
    seems the sectionpart is wrong

--- dev/sample/ch02_07/main.py
+++ dev/sample/ch02_07/main.py
@@ -11,30 +11,22 @@ from kivy.uix.button import Button
 class PythoniOS(App):
 		icon = 'icon.png'
 		title = 'ST.Python-iOS'
+		# def get_application_config(self):
+			# return 'C:\Users\stkd\Documents\GitHub\iphonedev\dev\sample\ch02_07\title.ini'
+		def builf_config(self,  config):
+			config.setdefaults( 'section_1' , {'key_1':'123',  'key_2':'456'})
 
-		def on_start(self):
-			print '-------------------'
-			print 'on_start'
-			print '-------------------'
-		def on_stop(self):
-			print '-------------------'
-			print 'on_stop'
-			print '-------------------'
 		def on_pause(self):
-			print '-------------------'
-			print 'pause'
-			print '-------------------'
+
 			return True
-		def on_resume(self):
-			print '-------------------'
-			print 'resume'
-			print '-------------------'
+			
+		
+			
 		def build(self):
 			print '-------------------'
+			print config.getdefaults('section_1')
 			print self.get_application_config()
-			print self.directory
-			print self.name
-			print self.get_application_icon()
+			print self.config.get( 'section_1' , 'key_1' )
 			print '-------------------'
 			return Button(text=u'Hello Worldソウか' , font_name = 'C:\Users\stkd\Documents\GitHub\iphonedev\dev\sample\ch02_07\\01FLOPDESIGN.ttf')
 			
